565191738f7879b2ff86dd8e91ea527a8214dcd0
jackson <jacksonjay916@gmail.com>
2025-08-15 12:53:01 +0300

    Add initial user and organization models, serializers, and empty admin, urls, and views files

---
+++ prodpulse-api/apps/users/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class Organization(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class User(AbstractUser):
+    organization = models.ForeignKey(
+        Organization, on_delete=models.CASCADE, related_name="members"
+    )
+    # any custom fields here
---
+++ prodpulse-api/apps/users/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import User, Organization
+
+class OrganizationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Organization
+        fields = "__all__"
+
+class UserSerializer(serializers.ModelSerializer):
+    organization = OrganizationSerializer(read_only=True)
+
+    class Meta:
+        model = User
+        exclude = ("password",)
