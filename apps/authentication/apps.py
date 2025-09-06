3796af8f6222e43ad759ab9e413cb88a5e8e37e8
jackson <jacksonjay916@gmail.com>
2025-09-06 22:06:15 +0300

    initialized authentication

--- apps/authentication/apps.py
+++ apps/authentication/apps.py
@@ -3,7 +3,3 @@ from django.apps import AppConfig
 class AuthenticationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.authentication"
-
-    def ready(self):
-        # Optional: register JWT signal hooks or auth validators
-        pass
