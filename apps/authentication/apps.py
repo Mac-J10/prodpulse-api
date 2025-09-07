bb40a4eec2b04618425bbc367b1cdafad7b6639b
jackson <jacksonjay916@gmail.com>
2025-09-07 07:05:54 +0300

    apps.py for authentication modified to enable full loading by django

--- apps/authentication/apps.py
+++ apps/authentication/apps.py
@@ -1,5 +1,11 @@
+# apps/authentication/apps.py
+
 from django.apps import AppConfig
 
 class AuthenticationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.authentication"
+
+    def ready(self):
+        # Only here do we import things that rely on Django being fully loaded
+        from . import signals
