37d8c858fcd61c0e6bb30664b310e0dee70a67dd
jackson <jacksonjay916@gmail.com>
2025-09-07 15:11:37 +0300

    fix: update import statement for RefreshToken in views.py

--- prodpulse_api/apps/authentication/views.py
+++ prodpulse_api/apps/authentication/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from .serializers import RegistrationSerializer, LogoutSerializer
-from rest_framework_simplejwt.views import RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class LogoutView(APIView):
     permission_classes = [IsAuthenticated]
